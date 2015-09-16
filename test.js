8fdf11eedbea299bb738197019f7da4969fe1f6f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-16 10:05:54 +0200

    Add test

--- test.js
+++ test.js
@@ -17,6 +17,39 @@ test('Doesn’t change the `tape` instance', (is) => {
   is.end();
 });
 
+test('Doesn’t change the `tape` API', (is) => {
+  const localTape = tape.createHarness({exit: false});
+  const localTest = tapeCss_(localTape);
+
+  const tapStream = localTape.createStream({objectMode: true});
+  tapStream.on('data', console.log);  // TODO: Test the output.
+
+  localTest('1', (localIs) => {
+    is.fail('`tape.only` works as before');
+    localIs.end();
+  });
+
+  localTest.only('2', (localIs) => {
+    localIs.pass();
+    localIs.ok(true);
+    localIs.notOk(false);
+    localIs.equal(1, 1);
+    localIs.deepEqual([1], [1]);
+    localIs.throws(() => { throw new Error('whatever'); });
+
+    localIs.skip('Whooah!');
+
+    localIs.fail();
+    localIs.ok(false);
+    localIs.deepEqual([1], [2]);
+    localIs.doesNotThrow(() => { throw new Error('whatever'); });
+
+    localIs.end();
+  });
+
+  is.end();
+});
+
 test.skip('Adds and removes DOM', (is) => {
   is.end();
 });
