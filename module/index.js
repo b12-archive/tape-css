6656e92a6a6b1b7988ebd402a3d56e4ef86df18e
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-09-16 13:58:34 +0200

    Initial implementation

--- module/index.js
+++ module/index.js
@@ -1,3 +1,30 @@
+const assign = require('object-assign');
+const find = require('array-find');
+const drop = require('this-drop');
+
 export default (tape) => {
-  return tape;
+  const tapeCss = (...args) => {
+    // Determine arguments – based on
+    // https://github.com/substack/tape/blob/aadcf4a9/lib/test.js .
+    const name = find(args, (arg) => typeof arg === 'string');
+    const options = find(args, (arg) => typeof arg === 'object') || {};
+    const callback = find(args, (arg) => typeof arg === 'function');
+
+    const {dom} = options;
+
+    const wrappedCallback = (is) => {
+      if (dom) document.body.appendChild(dom);
+      callback(is);
+      if (dom) document.body.removeChild(dom);
+    };
+
+    tape(
+      name,
+      options::drop(['dom']),
+      wrappedCallback
+    );
+  };
+  assign(tapeCss, tape);
+
+  return tapeCss;
 };
